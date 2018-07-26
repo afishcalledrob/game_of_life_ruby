97709672a9a529d26633934b9baa1eb7ad257d6e
Robert <fishwickrob@live.com>
2018-07-26 16:23:39 +0000

    Update method generates a next_grid

--- spec/grid_manager_spec.rb
+++ spec/grid_manager_spec.rb
@@ -3,7 +3,7 @@ describe GridManager do
   let(:subject) { GridManager.new(current_grid, next_grid) }
   let(:current_grid) { spy :current_grid }
   let(:next_grid) { spy :next_grid }
-  it 'should respond to the method "current_grid"' do
+  it 'should respond to the attribute method "current_grid"' do
       expect(subject).to respond_to(:current_grid)
   end
  
@@ -12,7 +12,15 @@ describe GridManager do
     expect(current_grid).to have_received(:generate)
   end
   
-  it 'should respond to the method "next_grid"' do
+  it 'should respond to the attribute method "next_grid"' do
       expect(subject).to respond_to(:next_grid)
   end
+  
+ it 'should respond to the method "update" by generating a next_grid' do
+     subject.start(10, 10) 
+     subject.update
+     expect(next_grid).to have_received(:generate)
+     
+  end
+  
 end
