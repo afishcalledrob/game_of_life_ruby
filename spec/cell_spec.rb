0e0bf818d905279a6817b834848baa0270ac10ed
Robert <fishwickrob@live.com>
2018-10-14 15:05:40 +0000

    testing default status of alive is false

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,4 +5,8 @@ describe Cell do
     subject(:cell) { described_class.new }
     
     it { is_expected.to respond_to(:alive) }
+    
+    it 'should have a default alive status of false' do
+       expect(subject.alive).to eq false 
+    end
 end
